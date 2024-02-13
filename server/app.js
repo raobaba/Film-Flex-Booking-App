154fa922c7c4b0799f949b4e7cc3604a607d71fb
Rajan Kumar <raorajan9576@gmail.com>
2024-02-13 16:40:49 +0530

    folder structure created , backend running

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const connection = require("./config/db.js");
+// const userRouter = require("./routes/user.route.js");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+connection();
+// app.use("/api/v1", userRouter);
+
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
+});
+
+module.exports = app;
