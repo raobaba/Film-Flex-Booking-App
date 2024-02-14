1da5035320538e2674a514e63c783a2e1b52ecfd
Rajan Kumar <raorajan9576@gmail.com>
2024-02-14 17:49:34 +0530

    responsive navbar and sidebar created

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+// Navbar.js
+
+import React, { useState, useRef, useEffect } from "react";
 import "./Navbar.css";
 import { SlMenu } from "react-icons/sl";
-
 import { GoSearch } from "react-icons/go";
 import { FaLocationDot } from "react-icons/fa6";
 
 function Navbar() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setSidebarOpen(false);
+  };
+
+  const sidebarRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+        closeSidebar();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [sidebarRef]);
+
   return (
     <div className="navbar-container">
       <div className="left-side">
@@ -27,10 +54,20 @@ function Navbar() {
         <div className="sign-in">
           <button>Sign In</button>
         </div>
-        <div className="menu-icon">
+        <div className="menu-icon" onClick={toggleSidebar}>
           <SlMenu />
         </div>
       </div>
+
+      {/* Sidebar */}
+      {isSidebarOpen && (
+        <div className="sidebar" ref={sidebarRef}>
+          {/* Sidebar content goes here */}
+          <p>Location</p>
+          <p>Sign In</p>
+          {/* Add more sidebar items as needed */}
+        </div>
+      )}
     </div>
   );
 }
