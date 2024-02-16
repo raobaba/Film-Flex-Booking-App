c4708f41edf6a35b87b89c34e5650ca35374ab19
Rajan Kumar <raorajan9576@gmail.com>
2024-02-16 14:09:08 +0530

    react slider is created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import ReactSlider from './components/ReactSlider/ReactSlider';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <ReactSlider/>
     </>
   );
 }
---
+++ client/src/components/ReactSlider/ReactSlider.jsx
@@ -0,0 +1,25 @@
+// ReactSlider.js
+import React from 'react';
+import './ReactSlider.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+function ReactSlider() {
+  return (
+    <div>
+      <Carousel autoPlay showThumbs={false} showStatus={false} infiniteLoop>
+        <div>
+          <img src="https://assets-in.bmscdn.com/promotions/cms/creatives/1706382336630_web.jpg" alt="Slide 1" />
+        </div>
+        <div>
+          <img src="https://assets-in.bmscdn.com/promotions/cms/creatives/1707978967837_premiereweb.jpg" alt="Slide 2" />
+        </div>
+        <div>
+          <img src="https://assets-in.bmscdn.com/promotions/cms/creatives/1706382336630_web.jpg" alt="Slide 3" />
+        </div>
+      </Carousel>
+    </div>
+  );
+}
+
+export default ReactSlider;
